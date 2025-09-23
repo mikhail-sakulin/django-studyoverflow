b960dc1ecf5200c73e82ad63f2cadae6d38a8638
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-09-23 20:49:31 +0700

    feat(posts): change slug and add get_absolute_url

--- studyoverflow/posts/models.py
+++ studyoverflow/posts/models.py
@@ -1,7 +1,13 @@
+from typing import Final
+
 from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 
 from .utils import translit_rus_to_eng
+
+
+MAX_TITLE_SLUG_LENGTH: Final = 255  # максимальная длина заголовка и slug
 
 
 class Post(models.Model):
@@ -9,28 +15,22 @@ class Post(models.Model):
     Модель поста приложения posts.
 
     Атрибуты:
-        title (CharFiled): Заголовок поста.
-        slug (SlugField): Уникальный URL-идентификатор (slug).
+        title (CharField): Заголовок поста.
+        slug (SlugField): Человекопонятная часть уникального URL-идентификатора /slug/pk/.
         content (TextField): Текст поста.
         time_create (DateTimeField): Время создания поста.
         time_update (DateTimeField): Время последнего обновления поста.
 
     Методы:
-        save(): Переопределяет стандартный метод сохранения объекта в БД для генерации slug.
+        save(*args, **kwargs): Переопределяет стандартный метод сохранения объекта в БД для
+            генерации slug.
+        get_absolute_url(): Возвращает уникальный URL для поста на основе slug и pk.
     """
 
-    MAX_TITLE_LENGTH = 255  # максимальная длина заголовка
-
-    title = models.CharField(max_length=MAX_TITLE_LENGTH, verbose_name="Заголовок")
-
-    slug = models.SlugField(
-        max_length=MAX_TITLE_LENGTH, unique=True, db_index=True, verbose_name="Slug"
-    )
-
+    title = models.CharField(max_length=MAX_TITLE_SLUG_LENGTH, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=MAX_TITLE_SLUG_LENGTH, verbose_name="Slug")
     content = models.TextField(blank=True, verbose_name="Текст поста")
-
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
-
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
 
     def __str__(self):
@@ -46,34 +46,18 @@ class Post(models.Model):
         """
         Переопределение метода save.
 
-        Сохраняет объект в БД с автоматической генерацией уникального slug.
+        Сохраняет объект в БД с автоматической генерацией slug.
 
-        Slug генерируется на основе title. Если такой slug уже существует,
-        к нему добавляется уникальный числовой суффикс.
-
-        Длина slug не превышает 255 символов, при необходимости базовая
-        часть slug обрезается.
+        Slug генерируется на основе title с обрезкой в случае превышения максимальной длины.
         """
 
-        if not self.slug:
-            base_slug = slugify(translit_rus_to_eng(self.title))
-            slug = base_slug[: self.MAX_TITLE_LENGTH]
-            counter = 1
-
-            while Post.objects.filter(slug=slug).exists():
-                counter += 1
-                counter_str = str(counter)
-
-                # Вычисляется максимальная длина базовой части slug
-                max_base_slug_length = self.MAX_TITLE_LENGTH - len(counter_str) - 1  # -1 для дефиса
-
-                if len(base_slug) > max_base_slug_length:
-                    truncated_base = base_slug[:max_base_slug_length]
-                else:
-                    truncated_base = base_slug
-
-                slug = f"{truncated_base}-{counter_str}"
-
-            self.slug = slug
-
+        base_slug = slugify(translit_rus_to_eng(self.title))
+        slug = base_slug[:MAX_TITLE_SLUG_LENGTH]
+        self.slug = slug
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        """
+        Возвращает уникальный URL для поста на основе slug и pk.
+        """
+        return reverse("post", kwargs={"post_slug": self.slug, "pk": self.pk})
