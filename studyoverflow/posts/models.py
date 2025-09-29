d172e23c26c3f47833ef874b6e3e7079488faca4
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-09-29 21:54:44 +0700

    feat(posts): add tag models for post

--- studyoverflow/posts/models.py
+++ studyoverflow/posts/models.py
@@ -3,9 +3,56 @@ from typing import Final
 from django.db import models
 from django.urls import reverse
 from posts.services.domain import generate_slug
+from taggit.managers import TaggableManager
+from taggit.models import GenericTaggedItemBase, TagBase
 
 
-MAX_TITLE_SLUG_LENGTH: Final = 255  # максимальная длина заголовка и slug
+MAX_TITLE_SLUG_LENGTH_POST: Final = 255  # максимальная длина заголовка и slug поста
+MAX_NAME_SLUG_LENGTH_TAG: Final = 50  # максимальная длина заголовка и slug тега
+
+
+class LowercaseTag(TagBase):
+    """
+    Пользовательская модель тега (наследование от taggit.TagBase).
+
+    Атрибуты:
+        name (CharField): Имя тега.
+        slug (SlugField): Человекопонятная часть уникального URL-идентификатора /slug/pk/.
+
+    Методы:
+        - save(*args, **kwargs):
+            - преобразует имя тега в нижний регистр перед сохранением с удалением лишних пробелов,
+            - генерирует slug;
+        - get_absolute_url(): Возвращает уникальный URL для тега на основе slug и pk.
+    """
+
+    name = models.CharField(max_length=MAX_NAME_SLUG_LENGTH_TAG, unique=True, verbose_name="Тег")
+    slug = models.SlugField(max_length=MAX_NAME_SLUG_LENGTH_TAG, verbose_name="Slug")
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.strip().lower()
+        if not self.slug:
+            self.slug = generate_slug(self.name, max_length=MAX_NAME_SLUG_LENGTH_TAG)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        """
+        Возвращает уникальный URL для тега на основе slug и pk.
+        """
+        return reverse("tag", kwargs={"tag_slug": self.slug, "pk": self.pk})
+
+
+class TaggedPost(GenericTaggedItemBase):
+    """
+    Пользовательская промежуточная модель для связи
+    постов (Post) с тегами (LowercaseTag).
+    """
+
+    tag = models.ForeignKey(LowercaseTag, related_name="posts", on_delete=models.CASCADE)
 
 
 class Post(models.Model):
@@ -16,6 +63,7 @@ class Post(models.Model):
         title (CharField): Заголовок поста.
         slug (SlugField): Человекопонятная часть уникального URL-идентификатора /slug/pk/.
         content (TextField): Текст поста.
+        tag (TaggableManager): Теги поста.
         time_create (DateTimeField): Время создания поста.
         time_update (DateTimeField): Время последнего обновления поста.
 
@@ -25,9 +73,10 @@ class Post(models.Model):
         get_absolute_url(): Возвращает уникальный URL для поста на основе slug и pk.
     """
 
-    title = models.CharField(max_length=MAX_TITLE_SLUG_LENGTH, verbose_name="Заголовок")
-    slug = models.SlugField(max_length=MAX_TITLE_SLUG_LENGTH, verbose_name="Slug")
+    title = models.CharField(max_length=MAX_TITLE_SLUG_LENGTH_POST, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=MAX_TITLE_SLUG_LENGTH_POST, verbose_name="Slug")
     content = models.TextField(blank=True, verbose_name="Содержимое поста")
+    tags = TaggableManager(through=TaggedPost, verbose_name="Теги")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
 
@@ -52,7 +101,7 @@ class Post(models.Model):
         """
 
         if not self.slug:
-            self.slug = generate_slug(self.title, MAX_TITLE_SLUG_LENGTH)
+            self.slug = generate_slug(self.title, MAX_TITLE_SLUG_LENGTH_POST)
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
