e3e1ec416afa5efa5a52bd230fd9c228dd225792
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-10-05 21:29:39 +0700

    refactor(posts): add app_name and url prefix for posts

--- studyoverflow/posts/urls.py
+++ studyoverflow/posts/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from . import views
 
 
+app_name = "posts"
+
+
 urlpatterns = [
-    path("", views.index, name="home"),
-    path("posts/", views.PostListView.as_view(), name="post_list"),
-    path("create-post/", views.PostCreateView.as_view(), name="create_post"),
+    path("", views.PostListView.as_view(), name="list"),
+    path("create/", views.PostCreateView.as_view(), name="create"),
     path("users/", views.users, name="users"),
 ]
--- studyoverflow/studyoverflow/urls.py
+++ studyoverflow/studyoverflow/urls.py
@@ -1,20 +1,4 @@
-"""вк
-URL configuration for studyoverflow project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+import posts.views
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -23,7 +7,8 @@ from studyoverflow import settings
 
 
 urlpatterns = [
+    path("", posts.views.index, name="home"),
     path("admin/", admin.site.urls),
-    path("", include("posts.urls")),
+    path("posts/", include("posts.urls")),
     path("favicon.ico", RedirectView.as_view(url=settings.STATIC_URL + "img/favicon.ico")),
 ]
