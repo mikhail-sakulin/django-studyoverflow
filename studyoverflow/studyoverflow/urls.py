bac14d4399a8277d11e73634d1a8e4b3befd2d34
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-10-07 18:14:40 +0700

    chore: add urlpatterns for media files

--- studyoverflow/studyoverflow/urls.py
+++ studyoverflow/studyoverflow/urls.py
@@ -1,4 +1,6 @@
-import posts.views
+import os
+
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -11,5 +13,11 @@ urlpatterns = [
     path("", include("navigation.urls")),
     path("posts/", include("posts.urls")),
     path("users/", include("users.urls")),
-    path("favicon.ico", RedirectView.as_view(url=settings.STATIC_URL + "img/favicon.ico")),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=settings.STATIC_URL + os.path.join("img", "favicon.ico")),
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
