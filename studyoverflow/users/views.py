5dab33acb127647647f3379e00276da300e2ec5d
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-10-07 17:57:25 +0700

    fix(users): add extra_context for menu

--- studyoverflow/users/views.py
+++ studyoverflow/users/views.py
@@ -3,3 +3,4 @@ from django.views.generic import TemplateView
 
 class UsersTemplateView(TemplateView):
     template_name = "users/users.html"
+    extra_context = {"section_of_menu_selected": "users:list"}
