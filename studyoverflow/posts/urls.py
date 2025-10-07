ccdb789f018cadfbca7efb264998aad1a81610ec
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-10-07 17:57:53 +0700

    refactor(posts): remove unused code

--- studyoverflow/posts/urls.py
+++ studyoverflow/posts/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path("", views.PostListView.as_view(), name="list"),
     path("create/", views.PostCreateView.as_view(), name="create"),
     path("<int:pk>/<slug:slug>/", views.PostDetailView.as_view(), name="detail"),
-    path("users/", views.users, name="users"),
 ]
--- studyoverflow/posts/views.py
+++ studyoverflow/posts/views.py
@@ -1,15 +1,7 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView
 from posts.forms import PostCreateForm
 from posts.models import LowercaseTag, Post
-
-
-def index(request):
-    """
-    Главная страница сайта.
-    """
-    return render(request, "posts/index.html")
 
 
 class PostListView(ListView):
@@ -50,10 +42,3 @@ class PostDetailView(DetailView):
     model = Post
     template_name = "posts/post_detail.html"
     context_object_name = "post"
-
-
-def users(request):
-    """
-    Страница демонстрации всех пользователей.
-    """
-    return render(request, "posts/users.html", context={"section_of_menu_selected": "posts:users"})
