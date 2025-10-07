0cc7de5ead7db35441780680789e445e8c297923
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-10-07 21:56:39 +0700

    fix(posts): fix extra_context for PostListView

--- studyoverflow/posts/views.py
+++ studyoverflow/posts/views.py
@@ -9,11 +9,7 @@ class PostListView(ListView):
     template_name = "posts/post_list.html"
     context_object_name = "posts"
     paginate_by = 7
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["section_of_menu_selected"] = "posts:list"
-        return context
+    extra_context = {"section_of_menu_selected": "posts:list"}
 
 
 class PostCreateView(CreateView):
