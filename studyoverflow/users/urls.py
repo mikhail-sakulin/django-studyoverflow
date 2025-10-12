9a137b64cae2b022e94243beb25cf848565f64c3
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-10-12 23:21:00 +0700

    feat(users): add LogoutView

--- studyoverflow/users/urls.py
+++ studyoverflow/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
@@ -10,4 +11,5 @@ urlpatterns = [
     path("", views.UsersTemplateView.as_view(), name="list"),
     path("register/", views.UserRegisterView.as_view(), name="register"),
     path("login/", views.UserLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
