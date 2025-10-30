bac1f4a72379227b2d2e338fcbcdd194ee2d6ecc
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-10-30 19:01:22 +0700

    feat(users): add users:profile url-path

--- studyoverflow/users/urls.py
+++ studyoverflow/users/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("register/", views.UserRegisterView.as_view(), name="register"),
     path("login/", views.UserLoginView.as_view(), name="login"),
     path("logout/", views.CustomLogoutView.as_view(), name="logout"),
+    path("profile/", views.UserProfileUpdateView.as_view(), name="profile"),
 ]
