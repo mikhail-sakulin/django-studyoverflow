e98e8b0b09ff34c8c8e45c79a4645658a2badc0e
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-11-12 19:27:23 +0700

    feat(comment): add comment views with HTMX support and edit url paths

--- studyoverflow/posts/urls.py
+++ studyoverflow/posts/urls.py
@@ -12,13 +12,28 @@ urlpatterns = [
     path("<int:pk>/<slug:slug>/", views.PostDetailView.as_view(), name="detail"),
     path("edit/<int:pk>/<slug:slug>/", views.PostUpdateView.as_view(), name="edit"),
     path(
-        "<int:post_pk>/<slug:post_slug>/comment/create/",
-        views.CommentCreateView.as_view(),
-        name="comment_create",
+        "<int:post_pk>/<slug:post_slug>/comments/",
+        views.CommentListView.as_view(),
+        name="comment_list",
     ),
     path(
-        "<int:post_pk>/<slug:post_slug>/comment/<int:comment_pk>/update/",
+        "<int:post_pk>/<slug:post_slug>/comments/root/create/",
+        views.CommentRootCreateView.as_view(),
+        name="comment_root_create",
+    ),
+    path(
+        "<int:post_pk>/<slug:post_slug>/comments/child/create/",
+        views.CommentChildCreateView.as_view(),
+        name="comment_child_create",
+    ),
+    path(
+        "<int:post_pk>/<slug:post_slug>/comments/<int:comment_pk>/update/",
         views.CommentUpdateView.as_view(),
         name="comment_update",
+    ),
+    path(
+        "<int:post_pk>/<slug:post_slug>/comments/<int:comment_pk>/delete/",
+        views.CommentDeleteView.as_view(),
+        name="comment_delete",
     ),
 ]
--- studyoverflow/posts/views.py
+++ studyoverflow/posts/views.py
@@ -1,15 +1,19 @@
-from typing import Any
+import json
 
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Prefetch
-from django.http import HttpRequest, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, ListView, UpdateView
-from posts.forms import CommentCreateForm, PostCreateForm
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from posts.forms import CommentCreateForm, CommentUpdateForm, PostCreateForm
 from posts.models import Comment, Post
-from posts.services.infrastructure import PostTagMixin
+from posts.services.infrastructure import (
+    CommentGetMethodMixin,
+    HTMXHandle404Mixin,
+    LoginRequiredHTMXMixin,
+    PostTagMixin,
+)
 
 
 class PostListView(ListView):
@@ -20,7 +24,7 @@ class PostListView(ListView):
     extra_context = {"section_of_menu_selected": "posts:list"}
 
 
-class PostCreateView(PostTagMixin, LoginRequiredMixin, CreateView):
+class PostCreateView(PostTagMixin, LoginRequiredHTMXMixin, CreateView):
     """
     Класс-представление для создания нового поста.
     """
@@ -46,37 +50,43 @@ class PostDetailView(DetailView):
         context["comment_form"] = CommentCreateForm()
         return context
 
-    def get_queryset(self):
-        return Post.objects.prefetch_related(
-            Prefetch(
-                "comments",
-                queryset=Comment.objects.roots().prefetch_related(
-                    Prefetch("child_comments", queryset=Comment.objects.order_by("time_create"))
-                ),
-            )
-        )
 
-
-class PostUpdateView(PostTagMixin, UpdateView):
+class PostUpdateView(LoginRequiredHTMXMixin, PostTagMixin, UpdateView):
     model = Post
     form_class = PostCreateForm
     template_name = "posts/post_edit.html"
     context_object_name = "post"
 
 
-class CommentBaseView(LoginRequiredMixin):
+class CommentListView(ListView):
+    model = Comment
+    template_name = "posts/comments/comment_list.html"
+    context_object_name = "root_comments"
+
+    def _get_post_object(self):
+        if not hasattr(self, "_post_object"):
+            post_pk = self.kwargs.get("post_pk")
+            self._post_object = get_object_or_404(Post, pk=post_pk)
+        return self._post_object
+
+    def get_queryset(self):
+        post = self._get_post_object()
+        return post.comments.roots().prefetch_related(
+            Prefetch("child_comments", queryset=Comment.objects.order_by("-time_create"))
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self._get_post_object()
+        context["post"] = post
+        context["comment_form"] = CommentCreateForm()
+        return context
+
+
+class CommentRootCreateView(LoginRequiredHTMXMixin, CommentGetMethodMixin, CreateView):
     model = Comment
     form_class = CommentCreateForm
-
-    kwargs: dict[str, Any]
-    request: HttpRequest
-
-    def _redirect_to_post_detail(self):
-        """
-        Редирект на страницу поста.
-        """
-        post = get_object_or_404(Post, id=self.kwargs.get("post_pk"))
-        return HttpResponseRedirect(post.get_absolute_url())
+    template_name = "posts/comments/_comment_root_form.html"
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
@@ -85,31 +95,128 @@ class CommentBaseView(LoginRequiredMixin):
         kwargs["post"] = get_object_or_404(Post, id=post_id)
         return kwargs
 
-    def form_invalid(self, form):
-        # Ошибки формы в messages, если они имеются
-        for field, errors in form.errors.items():
-            for error in errors:
-                messages.error(self.request, f'Ошибка в "{form.fields[field].label}": {error}')
-
-        # Редирект обратно на страницу поста
-        return self._redirect_to_post_detail()
-
-    def get(self, request, *args, **kwargs):
-        return self._redirect_to_post_detail()
-
-    def get_success_url(self):
-        return self.object.post.get_absolute_url()
-
-
-class CommentCreateView(CommentBaseView, CreateView):
-    model = Comment
-    form_class = CommentCreateForm
+    def get_context_data(self, form, form_valid=True, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.pop("form")
+        reply_to = form.cleaned_data.get("reply_to")
+        context = {
+            "post": form.post,
+            "comment": reply_to,
+            "comment_form": self.form_class() if form_valid else form,
+        }
+        return context
 
     def form_valid(self, form):
         form.instance.post = form.post
         form.instance.author = self.request.user
-        return super().form_valid(form)
+        self.object = form.save()
+
+        context = self.get_context_data(form, form_valid=True)
+
+        response = render(self.request, self.template_name, context)
+        response["HX-Trigger"] = "commentsUpdated, commentRootFormSuccess"
+        return response
+
+    def form_invalid(self, form):
+        context = self.get_context_data(form, form_valid=False)
+
+        response = render(self.request, self.template_name, context)
+        response["HX-Trigger"] = "commentRootFormError"
+        return response
 
 
-class CommentUpdateView(CommentBaseView, UpdateView):
+class CommentChildCreateView(CommentRootCreateView):
+    template_name = "posts/comments/_comment_child_form.html"
+
+    def _add_errors_to_messages(self, form):
+        for field, errors in form.errors.items():
+            for error in errors:
+                if field == "__all__":
+                    # ошибки на уровне формы
+                    messages.error(self.request, f"Ошибка: {error}")
+                else:
+                    # ошибки конкретного поля
+                    messages.error(self.request, f'Ошибка в "{form.fields[field].label}": {error}')
+
+        # Редирект обратно на страницу поста
+        return form.post.get_absolute_url()
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        reply_to = form.cleaned_data.get("reply_to")
+
+        if not reply_to:
+            self._add_errors_to_messages(form)
+
+        # Определяется commentId для события
+        comment_id = reply_to.id
+
+        # Передаются данные в формате JSON для HX-Trigger
+        response["HX-Trigger"] = json.dumps(
+            {"commentsUpdated": {}, "commentChildFormSuccess": {"commentId": comment_id}}
+        )
+
+        return response
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+
+        reply_to = form.cleaned_data.get("reply_to")
+
+        if not reply_to:
+            self._add_errors_to_messages(form)
+            response["HX-Redirect"] = self.request.path
+            return response
+
+        comment_id = reply_to.id
+
+        response["HX-Trigger"] = json.dumps({"commentChildFormError": {"commentId": comment_id}})
+
+        return response
+
+
+class CommentUpdateView(
+    LoginRequiredHTMXMixin, HTMXHandle404Mixin, CommentGetMethodMixin, UpdateView
+):
+    model = Comment
+    form_class = CommentUpdateForm
     pk_url_kwarg = "comment_pk"
+    template_name = "posts/comments/_comment_card.html"
+
+    def get_context_data(self, form, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.pop("form")
+        context = {
+            "comment_form": form,
+            "comment": self.object,
+            "post": self.object.post,
+        }
+        return context
+
+    def form_valid(self, form):
+        self.object = form.save()
+        context = self.get_context_data(form)
+
+        response = render(self.request, self.template_name, context)
+        response["HX-Trigger"] = json.dumps({"commentUpdateSuccess": {"commentId": self.object.id}})
+        return response
+
+    def form_invalid(self, form):
+        context = self.get_context_data(form)
+
+        response = render(self.request, self.template_name, context)
+        response["HX-Trigger"] = json.dumps({"commentUpdateError": {"commentId": self.object.id}})
+        return response
+
+
+class CommentDeleteView(
+    LoginRequiredHTMXMixin, HTMXHandle404Mixin, CommentGetMethodMixin, DeleteView
+):
+    model = Comment
+    pk_url_kwarg = "comment_pk"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return HttpResponse(headers={"HX-Trigger": "commentsUpdated"})
