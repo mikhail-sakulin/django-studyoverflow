fa50fe348df0bd3e2b0615c69b2dc5b2f638abaa
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-11-29 18:39:40 +0700

    chore(settings): configure STATIC_ROOT and WhiteNoise

--- studyoverflow/studyoverflow/settings.py
+++ studyoverflow/studyoverflow/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -130,6 +131,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR / "media"
 
