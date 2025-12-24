4957121d415e272359bbcf412e33671d3764ed40
Mikhail Sakulin <mv.sakulin@yandex.ru>
2025-12-24 21:22:30 +0700

    feat(users): add styles and templates for display user roles
    - Добавлены стили и шаблоны для отображения ролей пользователей
    - Добавлен inclusion_tag для отображения роли

--- studyoverflow/users/templatetags/users_tags.py
+++ studyoverflow/users/templatetags/users_tags.py
@@ -1,8 +1,17 @@
 from django import template
+from django.contrib.auth import get_user_model
 from users.services.infrastructure import is_user_online
 
 
 register = template.Library()
+
+User = get_user_model()  # noqa: N806
+
+role_mapping = {
+    User.Role.ADMIN: ("role-admin", "Admin"),
+    User.Role.MODERATOR: ("role-moderator", "Moderator"),
+    User.Role.STAFF_VIEWER: ("role-staff", "Staff"),
+}
 
 
 @register.simple_tag
@@ -14,3 +23,13 @@ def online_status_tag(user):
         return True
 
     return user.last_seen
+
+
+@register.inclusion_tag("users/_role_badge.html")
+def user_role_badge(user):
+    css_role_badge_class, badge = role_mapping.get(user.role, (None, None))
+
+    return {
+        "css_role_badge_class": css_role_badge_class,
+        "badge": badge,
+    }
