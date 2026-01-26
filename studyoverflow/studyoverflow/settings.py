b30f278c350014f601bc6c10eaf1e00634cf4dfd
Mikhail Sakulin <mv.sakulin@yandex.ru>
2026-01-26 20:42:44 +0700

    feat(settings): add daphne to installed apps

--- studyoverflow/studyoverflow/settings.py
+++ studyoverflow/studyoverflow/settings.py
@@ -41,6 +41,7 @@ INTERNAL_IPS = ["127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
