03a2cb2a633bef7a5c94adcda71068933985f7ec
Mikhail Sakulin <mv.sakulin@yandex.ru>
2026-02-01 21:49:47 +0700

    feat(logging): add UserActivityMiddleware for requests logging

---
+++ studyoverflow/navigation/middleware.py
@@ -0,0 +1,30 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class UserActivityMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        skip_path_prefixes = ["/favicon.ico"]
+        if any(request.path.startswith(path) for path in skip_path_prefixes):
+            return self.get_response(request)
+
+        response = self.get_response(request)
+
+        logger.info(
+            "Отправлен запрос к ресурсу.",
+            extra={
+                "user_id": request.user.id if request.user.is_authenticated else None,
+                "username": request.user.username if request.user.is_authenticated else None,
+                "method": request.method,
+                "path": request.path,
+                "response_status_code": response.status_code,
+                "event_type": "request",
+            },
+        )
+
+        return response
