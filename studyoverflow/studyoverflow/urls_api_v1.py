eb1df99f7cc8abb329336524661f8bc8c4f30bde
Mikhail Sakulin <mv.sakulin@yandex.ru>
2026-07-10 02:00:44 +0700

    feat(openapi): add openapi urls

--- studyoverflow/studyoverflow/urls_api_v1.py
+++ studyoverflow/studyoverflow/urls_api_v1.py
@@ -1,8 +1,16 @@
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 
 urlpatterns = [
     path("", include("posts.api.urls")),
     path("", include("notifications.api.urls")),
     path("", include("users.api.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema")),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema")),
 ]
