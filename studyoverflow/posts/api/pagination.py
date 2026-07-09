fbf3182d32bb09766390592ee19c1d89ac9d04cd
Mikhail Sakulin <mv.sakulin@yandex.ru>
2026-07-10 01:49:30 +0700

    feat(posts): add PostCommentsPagination for api pagination

---
+++ studyoverflow/posts/api/pagination.py
@@ -0,0 +1,24 @@
+from navigation.api.pagination import CustomPageNumberPagination
+from rest_framework.response import Response
+
+
+class PostCommentsPagination(CustomPageNumberPagination):
+    """
+    Кастомная пагинация списка комментариев поста.
+
+    Помимо стандартной информации о пагинации возвращает общее количество
+    комментариев у поста, учитывая дочерние комментарии (ответы).
+    """
+
+    def get_paginated_response(self, data):
+        post = self.request.parser_context["view"].get_post()
+
+        return Response(
+            {
+                "parents_comments_count": self.page.paginator.count,
+                "all_comments_count": post.comments_count,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
