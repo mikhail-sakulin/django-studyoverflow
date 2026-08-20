b946ca6d5376c8358198cb5d4126c5a302cbf748
Mikhail Sakulin <mv.sakulin@yandex.ru>
2026-08-21 01:21:32 +0700

    refactor(users): move MODERATOR_PERMISSIONS and STAFF_PERMISSIONS from signals.py to services/moderation.py

--- studyoverflow/users/signals.py
+++ studyoverflow/users/signals.py
@@ -9,51 +9,13 @@ from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from users.services import delete_cache_user, get_user_avatar_paths_list, remove_user_offline
+from users.services.moderation import MODERATOR_PERMISSIONS, STAFF_PERMISSIONS
 from users.tasks import delete_files_from_storage_task
 
 
 UserModel = get_user_model()
 
 logger = logging.getLogger(__name__)
-
-
-# Пары (app_label, codename) прав, которые будет иметь группа "Moderators"
-MODERATOR_PERMISSIONS = [
-    # posts.Comment
-    ("posts", "add_comment"),
-    ("posts", "change_comment"),
-    ("posts", "delete_comment"),
-    ("posts", "moderate_comment"),
-    # posts.Post
-    ("posts", "add_post"),
-    ("posts", "change_post"),
-    ("posts", "delete_post"),
-    ("posts", "moderate_post"),
-    # users.User
-    ("users", "block_user"),
-]
-
-# Пары (app_label, codename) прав, которые получит группа "StaffViewers"
-STAFF_PERMISSIONS = [
-    ("account", "view_emailaddress"),
-    ("account", "view_emailconfirmation"),
-    ("admin", "view_logentry"),
-    ("auth", "view_group"),
-    ("auth", "view_permission"),
-    ("contenttypes", "view_contenttype"),
-    ("notifications", "view_notification"),
-    ("posts", "view_comment"),
-    ("posts", "view_like"),
-    ("posts", "view_tag"),
-    ("posts", "view_post"),
-    ("posts", "view_posttag"),
-    ("sessions", "view_session"),
-    ("sites", "view_site"),
-    ("socialaccount", "view_socialaccount"),
-    ("socialaccount", "view_socialapp"),
-    ("socialaccount", "view_socialtoken"),
-    ("users", "view_user"),
-]
 
 
 def _perms_queryset(pairs):
@@ -68,7 +30,8 @@ def _perms_queryset(pairs):
 
 def sync_default_groups(sender, **kwargs):
     """
-    Обработчик post_migrate: создаёт/обновляет группы Moderators и StaffViewers с нужными правами.
+    Обработчик сигнала post_migrate: создаёт/обновляет группы Moderators и
+    StaffViewers с нужными правами.
     """
     moderator_group, _ = Group.objects.get_or_create(name="Moderators")
     staff_group, _ = Group.objects.get_or_create(name="StaffViewers")
@@ -96,7 +59,7 @@ def delete_user_avatars_after_user_deleted(sender, instance, **kwargs):
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
     """
-    Сигнал, срабатывающий при успешной авторизации пользователя.
+    Обработчик сигнала, срабатывающий при успешной авторизации пользователя.
 
     Логирует факт входа в систему с записью данных пользователя.
     """
@@ -118,7 +81,7 @@ def log_user_login(sender, request, user, **kwargs):
 @receiver(user_signed_up)
 def log_user_signup(sender, request, user, **kwargs):
     """
-    Сигнал (из allauth), срабатывающий при регистрации нового пользователя.
+    Обработчик сигнала (из allauth), срабатывающий при регистрации нового пользователя.
 
     При регистрации через соцсети срабатывает автоматически, при регистрации по
     логину и паролю требует ручного вызова.
@@ -149,7 +112,7 @@ def log_user_signup(sender, request, user, **kwargs):
 @receiver(user_logged_out)
 def log_user_logout(sender, request, user, **kwargs):
     """
-    Сигнал, срабатывающий при выходе пользователя из системы.
+    Обработчик сигнала, срабатывающий при выходе пользователя из системы.
 
     Логирует выход пользователя из системы с сохранением данных пользователя.
     """
@@ -171,7 +134,7 @@ def log_user_logout(sender, request, user, **kwargs):
 @receiver(user_logged_out)
 def remove_user_offline_when_logged_out(sender, request, user, **kwargs):
     """
-    Сигнал, срабатывающий при выходе пользователя из системы.
+    Обработчик сигнала, срабатывающий при выходе пользователя из системы.
 
     Удаляет информацию о присутствии пользователя (online status) из Redis.
     """
@@ -181,7 +144,7 @@ def remove_user_offline_when_logged_out(sender, request, user, **kwargs):
 @receiver(post_delete, sender=UserModel)
 def log_user_deletion(sender, instance, **kwargs):
     """
-    Сигнал, срабатывающий после удаления пользователя.
+    Обработчик сигнала, срабатывающий после удаления пользователя.
 
     Логирует удаление записи аккаунта из БД.
     """
@@ -203,7 +166,7 @@ def log_user_deletion(sender, instance, **kwargs):
 @receiver(user_login_failed)
 def log_user_login_failed(sender, credentials, request, **kwargs):
     """
-    Сигнал, срабатывающий при неудачной попытке входа.
+    Обработчик сигнала, срабатывающий при неудачной попытке входа.
 
     Логирует попытку авторизации с указанием введенного логина
     для мониторинга.
