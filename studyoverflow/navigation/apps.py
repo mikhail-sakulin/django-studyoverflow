99c52c2338ffbe48842ae795e81de61ac23f78e6
Mikhail Sakulin <mv.sakulin@yandex.ru>
2026-08-21 01:26:01 +0700

    feat(navigation): sync Site domain with SITE_DOMAIN .env param on post_migrate

--- studyoverflow/navigation/apps.py
+++ studyoverflow/navigation/apps.py
@@ -1,4 +1,5 @@
-from django.apps import AppConfig
+from django.apps import AppConfig, apps
+from django.db.models.signals import post_migrate
 
 
 class NavigationConfig(AppConfig):
@@ -9,3 +10,15 @@ class NavigationConfig(AppConfig):
         """Выполняется после загрузки приложений."""
         # Кастомные схемы аутентификации, импорт для автоматической регистрации в Swagger
         import navigation.api.swagger_extensions  # noqa: F401
+
+        # Регистрация обработчиков сигналов после загрузки приложения.
+        from navigation import signals  # noqa: F401
+
+        # AppConfig приложения django.contrib.sites
+        sites_config = apps.get_app_config("sites")
+
+        post_migrate.connect(
+            signals.sync_site_domain,
+            sender=sites_config,
+            dispatch_uid="sync_site_domain",
+        )
